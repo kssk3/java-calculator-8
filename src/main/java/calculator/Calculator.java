a9d0c5d87dd693d9747af23bb4e821899ec05c9d
kss8014 <kss8014@daum.net>
2025-10-15 17:17:57 +0900

    feat : Calculator Class 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,33 +1,51 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
-    private static final int INIT_VALUE = 0;
+    private static final String REGEX = "//(.)\n(.*)";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String EXCEPTION_MESSAGE = "구분 문자와 양수만 입력 가능합니다.";
+    private static final int DEFAULT_VALUE = 0;
 
     private int value;
 
     public Calculator() {
-        this.value = INIT_VALUE;
+        this.value = DEFAULT_VALUE;
     }
 
     public void run() {
-        System.out.println("덧셈할 문자열을 입력해주세요.");
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        String[] splitedValue = input.split("");
-        process(splitedValue);
+        process(input);
+        System.out.println("결과 : " + getValue());
     }
 
-    private void process(String[] splitedValue) {
+    private void process(String input) {
+        Pattern pattern = Pattern.compile(REGEX, Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.matches()) {
+            String delimiter = Pattern.quote(matcher.group(1));
+            String line = matcher.group(2);
+            parseValue(delimiter, line);
+        } else {
+            parseValue(DEFAULT_DELIMITER, input);
+        }
+    }
+
+    private void parseValue(String delimiter, String line) {
+        String[] tokens = line.split(delimiter);
+        for (String token : tokens) {
+            if (token.isEmpty()) continue;
+            value += Integer.parseInt(token);
+        }
     }
 
     public int getValue() {
-        return this.value;
-    }
-
-    public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        cal.run();
+        return value;
     }
 }
