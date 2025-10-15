4956c2f2ac0908d210dbf03d130754bc9d58d608
kss8014 <kss8014@daum.net>
2025-10-15 15:51:23 +0900

    test: 테스트 통과하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,14 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        String test = "//;\n1;2;3";
+        Pattern pattern = Pattern.compile("//[^0-9]\n]");
+        String delimiter = "//[^0-9]\n]";
 
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,32 +17,39 @@ class CalculatorTest {
     void test() {
         Calculator calculator = new Calculator();
         calculator.run();
-        Assertions.assertEquals(1, calculator.getValue());
+        Assertions.assertEquals(6, calculator.getValue());
     }
 
     class Calculator {
 
-        private static final String DELIMITER = "[,:]";
+        private static final String REGEX = "//(.)\n(.*)";
+        private static final String DEFAULT_DELIMITER = ",|:";
+
         private static final int DEFAULT_VALUE = 0;
 
-        private List<Integer> numbers;
         private int value;
 
         public Calculator() {
-            this.numbers = new ArrayList<>();
             this.value = DEFAULT_VALUE;
         }
 
         public void run() {
-            String test = "1,2:3,4";
-            String[] splited = test.split(DELIMITER);
-            for (String s : splited) {
-                this.value += Integer.parseInt(s);
-            }
+            process();
         }
 
         public int getValue() {
             return value;
+        }
+
+        private void process() {
+            // "//;\n1;2;3" ; => 구분 문자
+            String input = "1,2,3";
+            String delimiter = DEFAULT_DELIMITER;
+
+            String[] numbers = input.split(delimiter);
+            for (String number : numbers) {
+                value += Integer.parseInt(number);
+            }
         }
     }
 }
