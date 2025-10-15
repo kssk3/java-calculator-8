ff31469573a9ad3d9f2249f06fae588d844cdc34
kss8014 <kss8014@daum.net>
2025-10-15 16:31:51 +0900

    feat : 커스텀 구분문자를 이용해서 테스트 통과하기

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -17,7 +17,7 @@ class CalculatorTest {
     void test() {
         Calculator calculator = new Calculator();
         calculator.run();
-        Assertions.assertEquals(6, calculator.getValue());
+        assertEquals(5, calculator.getValue());
     }
 
     class Calculator {
@@ -25,31 +25,44 @@ class CalculatorTest {
         private static final String REGEX = "//(.)\n(.*)";
         private static final String DEFAULT_DELIMITER = ",|:";
 
+        private static final String EXCEPTION_MESSAGE = "구분 문자와 양수만 입력 가능합니다.";
         private static final int DEFAULT_VALUE = 0;
 
         private int value;
 
         public Calculator() {
-            this.value = DEFAULT_VALUE;
-        }
-
-        public void run() {
-            process();
+            value = DEFAULT_VALUE;
         }
 
         public int getValue() {
             return value;
         }
 
+        public void run() {
+            process();
+        }
+
         private void process() {
             // "//;\n1;2;3" ; => 구분 문자
             String input = "//;\n1;2;3";
-            String delimiter = DEFAULT_DELIMITER;
 
-            String[] numbers = input.split(delimiter);
-            for (String number : numbers) {
-                value += Integer.parseInt(number);
+            String delimiter = DEFAULT_DELIMITER;
+            String numbers = "";
+
+            Pattern pattern = Pattern.compile(REGEX, Pattern.DOTALL);
+            Matcher matcher = pattern.matcher(input);
+
+            if(matcher.matches()){
+                delimiter = matcher.group(1);
+                numbers = matcher.group(2);
+            }
+
+            String[] tokens = numbers.split(delimiter);
+            for (String token : tokens) {
+                value +=  Integer.parseInt(token);
             }
         }
+
+
     }
 }
